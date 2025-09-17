c03cf97d878b1717fd8ff5f0ff31ad53e141d411
Fernando <ferxinii@users.noreply.github.com>
2025-09-17 21:15:15 +0200

    Everything is nice

--- example/compile_and_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf 14.mp4 15.mp4
-make
-./example
-open 14.mp4 15.mp4
--- gnuplotc.c
+++ gnuplotc.c
@@ -1,4 +1,4 @@
-#include "plot.h"
+#include "gnuplotc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
